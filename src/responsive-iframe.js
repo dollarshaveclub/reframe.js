897552f4e159c31a7a0073d2db47bafbe25f7916
Jeff Wainwright <jjwainwright2@gmail.com>
2016-07-07 16:14:01 -0700

    Adds first working version of responsive iframes

---
+++ src/responsive-iframe.js
@@ -0,0 +1,38 @@
+(function( window, factory ) {
+
+  ResponsiveIframe = window.ResponsiveIframe || {};
+  if ( 'undefined' === typeof ResponsiveIframe ) return console.log('Unmet Dependencies');
+  factory( ResponsiveIframe );
+
+})( this, function( utils ) {
+
+  window.ResponsiveIframe = function( 
+    responsiveIframe, 
+    excludedIframe
+  ) {
+  
+    var exclusionSelector = typeof( excludedIframe ) !== 'undefined' ? excludedIframe : null;
+    if ( typeof( excludedIframe ) !== 'undefined' ) {
+      responsiveIframe = document.querySelectorAll(responsiveIframe+':not('+excludedIframe+')');
+    } else {
+      responsiveIframe = document.querySelectorAll(responsiveIframe);
+    }
+    for (i = 0; i < responsiveIframe.length; ++i) {
+      var wrapper = document.createElement("div"),
+          divAdded = false;
+      wrapper.className += 'js-responsive-iframe';
+      responsiveIframe[i].removeAttribute('height');
+      responsiveIframe[i].removeAttribute('width');
+      responsiveIframe[i].removeAttribute('style');
+      if ( ! divAdded ) {
+        responsiveIframe[i].parentNode.insertBefore(wrapper, responsiveIframe[i]);
+        divAdded = true;
+      }
+      responsiveIframe[i].parentNode.removeChild(responsiveIframe[i]);
+      wrapper.appendChild(responsiveIframe[i]); 
+    }
+
+    return this;
+  }; 
+
+});
