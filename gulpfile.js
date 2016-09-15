4d83d6e3191b12de8bbbd990e36559300ba17865
Jeff Wainwright <jjwainwright2@gmail.com>
2016-09-15 12:34:30 -0700

    removes unneeded files for the package

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
-const sassGlob = require('gulp-sass-glob');
 const rename = require('gulp-rename');
-const purifyCss = require('gulp-purifycss');
 const cssNano = require('gulp-cssnano');
 const qunit = require('node-qunit-phantomjs');
 const babel = require('gulp-babel');
@@ -18,18 +16,9 @@ gulp.task('styles', () => {
     .pipe(gulp.dest('dist/'));
   gulp
     .src('src/styles/reframe.scss')
-    .pipe(sassGlob())
     .pipe(sass().on('error', sass.logError))
     .pipe(cssNano())
     .pipe(gulp.dest('dist/'));
-  return gulp
-    .src('src/styles/package-only/main.scss')
-    .pipe(sassGlob())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(purifyCss(['index.html']))
-    .pipe(cssNano())
-    .pipe(gulp.dest(''));
-
 });
 
 gulp.task('test', function() {
