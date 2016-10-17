4e1cb4248af10e8c5c7bc7761cf279a74d5abfec
Jeff Wainwright <jjwainwright2@gmail.com>
2016-10-17 10:31:37 -0700

    moves the tests folder out of src

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,8 @@ const gulp = require('gulp');
 const qunit = require('node-qunit-phantomjs');
 
 gulp.task('test', function() {
-  qunit('src/tests/reframe/index.html');
-  qunit('src/tests/noframe/index.html');
+  qunit('tests/reframe/index.html');
+  qunit('tests/noframe/index.html');
 });
 
 gulp.task('default', ['test']);
