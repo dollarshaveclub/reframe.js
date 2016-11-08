3385303c48d48d2bb1719258fffe36351ec820b3
Jeff Wainwright <jjwainwright2@gmail.com>
2016-11-07 22:30:37 -0800

    add minified files for reframe & noframe

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,34 @@
 const gulp = require('gulp');
+const head = require('gulp-header');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
 const qunit = require('node-qunit-phantomjs');
+
+const pkg = require('./package.json');
+const banner = ['/**',
+  ' * <%= pkg.name %> - <%= pkg.description %>',
+  ' * @version v<%= pkg.version %>',
+  ' * @link <%= pkg.homepage %>',
+  ' * @author <%= pkg.author %>',
+  ' * @license <%= pkg.license %> */',
+''].join('\n');
 
 gulp.task('test', function() {
   qunit('tests/reframe/index.html');
   qunit('tests/noframe/index.html');
 });
 
-gulp.task('default', ['test']);
+gulp.task('minify', function() {
+  gulp.src('dist/reframe.js')
+    .pipe(uglify())
+    .pipe(head(banner, { pkg }))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('dist/'));
+  gulp.src('dist/noframe.js')
+    .pipe(uglify())
+    .pipe(head(banner, { pkg }))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('default', ['test', 'minify']);
