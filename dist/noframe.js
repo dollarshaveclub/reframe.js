406ec1a4711da8032c2c1fb5d1fa879d4fc02105
Jeff Wainwright <jjwainwright2@gmail.com>
2017-03-29 14:43:54 -0700

    1.0.3

--- dist/noframe.js
+++ dist/noframe.js
@@ -1,7 +1,7 @@
 (function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-  typeof define === 'function' && define.amd ? define(factory) :
-  (global.noframe = factory());
+	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+	typeof define === 'function' && define.amd ? define(factory) :
+	(global.noframe = factory());
 }(this, (function () { 'use strict';
 
 function noframe(target, container) {
