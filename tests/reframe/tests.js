ca926b6d34bb31dff670537b969d5f06220a87c8
Jeff Wainwright <jjwainwright2@gmail.com>
2017-05-17 11:44:43 -0700

    adds `intrinsic ratio` test for `reframe`

--- tests/reframe/tests.js
+++ tests/reframe/tests.js
@@ -6,6 +6,8 @@ const iframes = '<iframe id="iframe" width="315" height="315" src="https://www.y
 test.insertAdjacentHTML('afterbegin', iframes);
 
 var idIframe = $('#iframe');
+var iframeWidth = idIframe.width();
+var iframeHeight = idIframe.height();
 // test that there can be tests
 QUnit.test('there is #test id', function(assert) {
   assert.equal(document.querySelectorAll('#test').length, 1, 'there is a #test <div>');
@@ -15,25 +17,6 @@ QUnit.test('reframe by #Id', function(assert) {
   reframe('#iframe');
   assert.equal($('.js-reframe #iframe').length, 1, 'there is 1 reframed <elements> with #iframe');
 });
-
-// test that the intrinsic ratio is working
-// QUnit.test('check to make sure that the reframe intrinsic ratio is working', function(assert) {
-//   // set up the ratios to be tested
-//   var $iframe = $('#iframe');
-//   // get the actual size of the iframe
-//   var iframeWidth = $iframe.width();
-//   var iframeHeight = $iframe.height();
-//   var iframeRatio = (iframeHeight * iframeWidth) / 100;
-//   console.log(iframeWidth, iframeHeight, iframeRatio);
-//   // get the relative size of the parent
-//   var $iframeParent = $iframe.parents('.js-reframe');
-//   var parentWidth = $iframeParent.css('width');
-//   var parentHeight = $iframeParent.css('padding-top');
-//   var parentRatio = (parentHeight * parentWidth) / 100;
-//   console.log(parentWidth, parentHeight, parentRatio);
-//   // compare that both ratios are working
-//   assert.equal(iframeRatio, parentRatio, 'reframe intrinsic ratio is working');
-// });
 
 // test that reframe.js works when selecting multiple elements with classes
 QUnit.test('reframe <elements> with multiple classes', function(assert) {
@@ -46,4 +29,18 @@ QUnit.test('reframe <elements> with multiple classes', function(assert) {
 QUnit.test('reframe an <element> and add a unique css class', function(assert) {
   reframe('.unique', 'js-test');
   assert.equal($('.js-test').length, 1, 'there is 1 iframe with the class .js-test');
+});
+
+// test that the intrinsic ratio is working
+$(window).on('load', function() {
+  QUnit.test('check to make sure that the reframe intrinsic ratio is working', function(assert) {
+    // see variables stored above (before reframe is run)
+    var iframeRatio = iframeHeight / iframeWidth;
+    // set up ratio after reframe has been run
+    var iframeOuterWidth = idIframe.outerWidth();
+    var iframeOuterHeight = idIframe.outerHeight();
+    var iframeOuterRatio = iframeOuterWidth / iframeOuterHeight;
+    // compare that both ratios are working
+    assert.equal(iframeRatio, iframeOuterRatio, 'reframe intrinsic ratio is working');
+  });
 });
