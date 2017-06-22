bcd27133f08d28b1a3ca798e146f9fa92bb4493f
Jeff Wainwright <jjwainwright2@gmail.com>
2017-06-22 01:47:11 -0700

    cleans up `reframe/noframe` method

--- dist/jquery.reframe.js
+++ dist/jquery.reframe.js
@@ -1,23 +1,23 @@
 (function (global, factory) {
-	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory() :
 	typeof define === 'function' && define.amd ? define(factory) :
-	(global.reframe = factory());
+	(factory());
 }(this, (function () { 'use strict';
 
-function reframer(target, cName) {
+function reframeEl(target, cName) {
   var frame = target;
-  var classname = cName || 'js-reframe';
-  var hasClass = frame.className.split(' ').indexOf(classname) !== -1;
+  var c = cName || 'js-reframe';
+  var hasClass = frame.className.split(' ').indexOf(c) !== -1;
   if (hasClass) return;
-  var height = frame.offsetHeight;
-  var width = frame.offsetWidth;
-  var padding = height / width * 100;
+  var h = frame.offsetHeight;
+  var w = frame.offsetWidth;
+  var padding = h / w * 100;
   var div = document.createElement('div');
-  div.className = classname;
-  var divStyle = div.style;
-  divStyle.position = 'relative';
-  divStyle.width = '100%';
-  divStyle.paddingTop = padding + '%';
+  div.className = c;
+  var divStyles = div.style;
+  divStyles.position = 'relative';
+  divStyles.width = '100%';
+  divStyles.paddingTop = padding + '%';
   var frameStyle = frame.style;
   frameStyle.position = 'absolute';
   frameStyle.width = '100%';
@@ -28,15 +28,15 @@ function reframer(target, cName) {
   frame.parentNode.removeChild(frame);
   div.appendChild(frame);
 }
-
 function reframe(target, cName) {
   var frames = typeof target === 'string' ? document.querySelectorAll(target) : target;
   if (!('length' in frames)) frames = [frames];
   for (var i = 0; i < frames.length; i += 1) {
     var frame = frames[i];
-    reframer(frame, cName);
+    reframeEl(frame, cName);
   }
 }
+
 if (typeof window !== 'undefined') {
   var plugin = window.$ || window.jQuery || window.Zepto;
   if (plugin) {
@@ -45,7 +45,5 @@ if (typeof window !== 'undefined') {
     };
   }
 }
-
-return reframe;
 
 })));
--- src/jquery.noframe.js
+++ src/jquery.noframe.js
@@ -1,14 +1,5 @@
 /* global document, window */
-import noframer from './noframer';
-
-export default function noframe(target, cName) {
-  let frames = (typeof target === 'string') ? document.querySelectorAll(target) : target;
-  if (!('length' in frames)) frames = [frames];
-  for (let i = 0; i < frames.length; i += 1) {
-    const frame = frames[i];
-    noframer(frame, cName);
-  }
-}
+import noframe from './noframe';
 
 if (typeof window !== 'undefined') {
   const plugin = window.$ || window.jQuery || window.Zepto;
