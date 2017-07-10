50e6c05ff7510f4829864b162717fd8e186651f8
Jeff Wainwright <jjwainwright2@gmail.com>
2017-07-10 16:45:05 -0700

    [update] removes `noframeEl/reframeEl function` in noframe/reframe (#50)

--- dist/jquery.noframe.js
+++ dist/jquery.noframe.js
@@ -4,40 +4,36 @@
 	(factory());
 }(this, (function () { 'use strict';
 
-function noframeEl(target, container) {
-  var frame = target;
-  var parent = frame.parentElement;
-  var h = frame.offsetHeight;
-  var w = frame.offsetWidth;
-  var styles = frame.style;
-  var maxW = w + 'px';
-  if (typeof container !== 'undefined') {
-    parent = document.querySelector(container);
-    maxW = window.getComputedStyle(parent, null).getPropertyValue('max-width');
-    styles.width = '100%';
-    styles.maxHeight = 'calc(' + maxW + ' * ' + h + '/' + w + ')';
-  } else {
-    styles.display = 'block';
-    styles.marginLeft = 'auto';
-    styles.marginRight = 'auto';
-    var fullW = maxW + 'px';
-    if (w > parent.offsetWidth) {
-      fullW = parent.offsetWidth;
-      styles.maxH = 'calc(' + fullW + 'px * ' + h + '/' + w + ')';
-    } else {
-      styles.maxH = 'calc(' + maxW + ' * ' + h + '/' + w + ')';
-    }
-    styles.width = fullW + 'px';
-  }
-  styles.height = 'calc(100vw * ' + h + '/' + w + ')';
-  styles.maxWidth = '100vw';
-}
 function noframe(target, container) {
   var frames = typeof target === 'string' ? document.querySelectorAll(target) : target;
   if (!('length' in frames)) frames = [frames];
   for (var i = 0; i < frames.length; i += 1) {
     var frame = frames[i];
-    noframeEl(frame, container);
+    var parent = frame.parentElement;
+    var h = frame.offsetHeight;
+    var w = frame.offsetWidth;
+    var styles = frame.style;
+    var maxW = w + 'px';
+    if (typeof container !== 'undefined') {
+      parent = document.querySelector(container);
+      maxW = window.getComputedStyle(parent, null).getPropertyValue('max-width');
+      styles.width = '100%';
+      styles.maxHeight = 'calc(' + maxW + ' * ' + h + '/' + w + ')';
+    } else {
+      styles.display = 'block';
+      styles.marginLeft = 'auto';
+      styles.marginRight = 'auto';
+      var fullW = maxW + 'px';
+      if (w > parent.offsetWidth) {
+        fullW = parent.offsetWidth;
+        styles.maxH = 'calc(' + fullW + 'px * ' + h + '/' + w + ')';
+      } else {
+        styles.maxH = 'calc(' + maxW + ' * ' + h + '/' + w + ')';
+      }
+      styles.width = fullW + 'px';
+    }
+    styles.height = 'calc(100vw * ' + h + '/' + w + ')';
+    styles.maxWidth = '100vw';
   }
 }
 
--- src/reframe.js
+++ src/reframe.js
@@ -1,5 +1,3 @@
-/* global document, window */
-
 /*
   reframe.js 🖼
   -------------
@@ -9,46 +7,41 @@
   -------------
   defines the height/width ratio of the targeted <element>
 */
-function reframeEl(target, cName) {
-  const frame = target;
-  const c = cName || 'js-reframe';
-  // makes sure reframe is not run more than 1x ✔️
-  const hasClass = frame.className.split(' ').indexOf(c) !== -1;
-  if (hasClass) return;
-
-  // general targeted <element> sizes
-  const h = frame.offsetHeight;
-  const w = frame.offsetWidth;
-  const padding = (h / w) * 100;
-
-  // created element <wrapper> of general reframed item
-  // => set necessary styles of created element <wrapper>
-  const div = document.createElement('div');
-  div.className = c;
-  const divStyles = div.style;
-  divStyles.position = 'relative';
-  divStyles.width = '100%';
-  divStyles.paddingTop = `${padding}%`;
-
-  // set necessary styles of targeted <element>
-  const frameStyle = frame.style;
-  frameStyle.position = 'absolute';
-  frameStyle.width = '100%';
-  frameStyle.height = '100%';
-  frameStyle.left = '0';
-  frameStyle.top = '0';
-
-  // reframe targeted <element>
-  frame.parentNode.insertBefore(div, frame);
-  frame.parentNode.removeChild(frame);
-  div.appendChild(frame);
-};
-
 export default function reframe(target, cName) {
   let frames = (typeof target === 'string') ? document.querySelectorAll(target) : target;
+  const c = cName || 'js-reframe';
   if (!('length' in frames)) frames = [frames];
   for (let i = 0; i < frames.length; i += 1) {
     const frame = frames[i];
-    reframeEl(frame, cName);
+    // makes sure reframe is not run more than 1x ✔️
+    const hasClass = frame.className.split(' ').indexOf(c) !== -1;
+    if (hasClass) return;
+
+    // general targeted <element> sizes
+    const h = frame.offsetHeight;
+    const w = frame.offsetWidth;
+    const padding = (h / w) * 100;
+
+    // created element <wrapper> of general reframed item
+    // => set necessary styles of created element <wrapper>
+    const div = document.createElement('div');
+    div.className = c;
+    const divStyles = div.style;
+    divStyles.position = 'relative';
+    divStyles.width = '100%';
+    divStyles.paddingTop = `${padding}%`;
+
+    // set necessary styles of targeted <element>
+    const frameStyle = frame.style;
+    frameStyle.position = 'absolute';
+    frameStyle.width = '100%';
+    frameStyle.height = '100%';
+    frameStyle.left = '0';
+    frameStyle.top = '0';
+
+    // reframe targeted <element>
+    frame.parentNode.insertBefore(div, frame);
+    frame.parentNode.removeChild(frame);
+    div.appendChild(frame);
   }
 }
