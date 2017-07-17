3f8252f2e0e852ca31f7d2dbcfe934b669993826
Jeff Wainwright <jjwainwright2@gmail.com>
2017-07-16 21:34:35 -0600

    updates readme

--- reframe.js
+++ reframe.js
@@ -6,37 +6,42 @@
 
 function reframe(target, cName) {
   var frames = typeof target === 'string' ? document.querySelectorAll(target) : target;
-  if (!('length' in frames)) {
-    frames = [frames];
-  }
+  if (!('length' in frames)) frames = [frames];
   var classname = cName || 'js-reframe';
+
   for (var i = 0; i < frames.length; i++) {
     var frame = frames[i];
-    var hasClass = frame.className.split(' ').indexOf(classname);
-    if (hasClass >= 0) return;
-    var div = document.createElement('div');
+    var hasClass = frame.className.split(' ').indexOf(classname) !== -1;
+    if (hasClass) return;
+
     var height = frame.offsetHeight;
     var width = frame.offsetWidth;
     var padding = height / width * 100;
-    div.style.paddingTop = padding + '%';
+
+    var div = document.createElement('div');
+    div.className = classname;
     div.style.position = 'relative';
-    div.style.width = frame.style.width = frame.style.height = '100%';
-    div.className += classname;
+    div.style.width = '100%';
+    div.style.paddingTop = padding + '%';
+
+    frame.style.position = 'absolute';
+    frame.style.width = frame.style.height = '100%';
+    frame.style.left = frame.style.top = '0';
+
     frame.parentNode.insertBefore(div, frame);
     frame.parentNode.removeChild(frame);
-    frame.style.left = frame.style.top = '0';
-    frame.style.position = 'absolute';
     div.appendChild(frame);
   }
 }
 
-var plugin = window.$ || window.jQuery || window.zepto;
-if (plugin) {
-  plugin.fn.extend({
-    reframe: function reframeFunc(cName) {
-      return reframe(this, cName);
-    }
-  });
+if (typeof window !== 'undefined') {
+  var plugin = window.$ || window.jQuery || window.Zepto;
+  if (plugin) {
+    plugin.fn.reframe = function reframePlugin(cName) {
+      reframe(this, cName);
+      return;
+    };
+  }
 }
 
 return reframe;
