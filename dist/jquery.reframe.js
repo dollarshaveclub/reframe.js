8b66a0d5fa0813333c878a37cb508784a186aba0
Jeff Wainwright <yowainwright@gmail.com>
2018-02-16 14:12:46 -0800

    [update] changes w & h check logic

--- dist/jquery.reframe.js
+++ dist/jquery.reframe.js
@@ -5,41 +5,35 @@
 }(this, (function () { 'use strict';
 
 function reframe(target, cName) {
-    var frames = typeof target === 'string' ? document.querySelectorAll(target) : target;
-    var c = cName || 'js-reframe';
-    if (!('length' in frames)) frames = [frames];
-    for (var i = 0; i < frames.length; i += 1) {
-        var frame = frames[i];
-        var hasClass = frame.className.split(' ').indexOf(c) !== -1;
-        if (hasClass) return;
-        var hAttribute = frame.getAttribute('height');
-        var wAttribute = frame.getAttribute('width');
-        var h = void 0;
-        var w = void 0;
-        if (hAttribute !== null && wAttribute !== null) {
-            h = parseInt(hAttribute);
-            w = parseInt(wAttribute);
-        } else {
-            h = frame.offsetHeight;
-            w = frame.offsetWidth;
-        }
-        var padding = h / w * 100;
-        var div = document.createElement('div');
-        div.className = c;
-        var divStyles = div.style;
-        divStyles.position = 'relative';
-        divStyles.width = '100%';
-        divStyles.paddingTop = padding + '%';
-        var frameStyle = frame.style;
-        frameStyle.position = 'absolute';
-        frameStyle.width = '100%';
-        frameStyle.height = '100%';
-        frameStyle.left = '0';
-        frameStyle.top = '0';
-        frame.parentNode.insertBefore(div, frame);
-        frame.parentNode.removeChild(frame);
-        div.appendChild(frame);
-    }
+  var frames = typeof target === 'string' ? document.querySelectorAll(target) : target;
+  var c = cName || 'js-reframe';
+  if (!('length' in frames)) frames = [frames];
+  for (var i = 0; i < frames.length; i += 1) {
+    var frame = frames[i];
+    var hasClass = frame.className.split(' ').indexOf(c) !== -1;
+    if (hasClass) return;
+    var hAttr = frame.getAttribute('height');
+    var wAttr = frame.getAttribute('width');
+    if (wAttr.toString('%') || frame.style.width.toString('%')) return;
+    var h = !hAttr ? frame.offsetHeight : hAttr;
+    var w = !wAttr ? frame.offsetWidth : hAttr;
+    var padding = h / w * 100;
+    var div = document.createElement('div');
+    div.className = c;
+    var divStyles = div.style;
+    divStyles.position = 'relative';
+    divStyles.width = '100%';
+    divStyles.paddingTop = padding + '%';
+    var frameStyle = frame.style;
+    frameStyle.position = 'absolute';
+    frameStyle.width = '100%';
+    frameStyle.height = '100%';
+    frameStyle.left = '0';
+    frameStyle.top = '0';
+    frame.parentNode.insertBefore(div, frame);
+    frame.parentNode.removeChild(frame);
+    div.appendChild(frame);
+  }
 }
 
 if (typeof window !== 'undefined') {
