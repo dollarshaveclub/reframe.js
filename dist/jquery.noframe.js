6f74d414906771dd4a074d753cd965a82dbc418e
Jeff Wainwright <yowainwright@gmail.com>
2018-08-28 16:19:46 -0700

    Greenkeeper/rollup plugin babel 4.0.1 (#117)
    * chore(package): update rollup-plugin-babel to version 4.0.1
    
    Closes #116
    
    * [update] updates babel/eslint config

--- dist/jquery.noframe.js
+++ dist/jquery.noframe.js
@@ -1,50 +1,50 @@
 (function (global, factory) {
-	typeof exports === 'object' && typeof module !== 'undefined' ? factory() :
-	typeof define === 'function' && define.amd ? define(factory) :
-	(factory());
+  typeof exports === 'object' && typeof module !== 'undefined' ? factory() :
+  typeof define === 'function' && define.amd ? define(factory) :
+  (factory());
 }(this, (function () { 'use strict';
 
-function noframe(target, container) {
-  var frames = typeof target === 'string' ? document.querySelectorAll(target) : target;
-  if (!('length' in frames)) frames = [frames];
-  for (var i = 0; i < frames.length; i += 1) {
-    var frame = frames[i];
-    var isContainerElement = typeof container !== 'undefined' && document.querySelector(container);
-    var parent = isContainerElement ? document.querySelector(container) : frame.parentElement;
-    var h = frame.offsetHeight;
-    var w = frame.offsetWidth;
-    var styles = frame.style;
-    var maxW = w + 'px';
-    if (isContainerElement) {
-      maxW = window.getComputedStyle(parent, null).getPropertyValue('max-width');
-      styles.width = '100%';
-      styles.maxHeight = 'calc(' + maxW + ' * ' + h + ' / ' + w + ')';
-    } else {
-      var maxH = void 0;
-      styles.display = 'block';
-      styles.marginLeft = 'auto';
-      styles.marginRight = 'auto';
-      var fullW = maxW;
-      if (w > parent.offsetWidth) {
-        fullW = parent.offsetWidth;
-        maxH = fullW * h / w;
-      } else maxH = w * (h / w);
-      styles.maxHeight = maxH + 'px';
-      styles.width = fullW;
+  function noframe(target, container) {
+    var frames = typeof target === 'string' ? document.querySelectorAll(target) : target;
+    if (!('length' in frames)) frames = [frames];
+    for (var i = 0; i < frames.length; i += 1) {
+      var frame = frames[i];
+      var isContainerElement = typeof container !== 'undefined' && document.querySelector(container);
+      var parent = isContainerElement ? document.querySelector(container) : frame.parentElement;
+      var h = frame.offsetHeight;
+      var w = frame.offsetWidth;
+      var styles = frame.style;
+      var maxW = w + "px";
+      if (isContainerElement) {
+        maxW = window.getComputedStyle(parent, null).getPropertyValue('max-width');
+        styles.width = '100%';
+        styles.maxHeight = "calc(" + maxW + " * " + h + " / " + w + ")";
+      } else {
+        var maxH = void 0;
+        styles.display = 'block';
+        styles.marginLeft = 'auto';
+        styles.marginRight = 'auto';
+        var fullW = maxW;
+        if (w > parent.offsetWidth) {
+          fullW = parent.offsetWidth;
+          maxH = fullW * h / w;
+        } else maxH = w * (h / w);
+        styles.maxHeight = maxH + "px";
+        styles.width = fullW;
+      }
+      var cssHeight = 100 * h / w;
+      styles.height = cssHeight + "vw";
+      styles.maxWidth = '100%';
     }
-    var cssHeight = 100 * h / w;
-    styles.height = cssHeight + 'vw';
-    styles.maxWidth = '100%';
   }
-}
 
-if (typeof window !== 'undefined') {
-  var plugin = window.$ || window.jQuery || window.Zepto;
-  if (plugin) {
-    plugin.fn.noframe = function noframePlugin(cName) {
-      noframe(this, cName);
-    };
+  if (typeof window !== 'undefined') {
+    var plugin = window.$ || window.jQuery || window.Zepto;
+    if (plugin) {
+      plugin.fn.noframe = function noframePlugin(cName) {
+        noframe(this, cName);
+      };
+    }
   }
-}
 
 })));
--- src/jquery.reframe.js
+++ src/jquery.reframe.js
@@ -4,7 +4,7 @@ import reframe from './reframe'
 if (typeof window !== 'undefined') {
   const plugin = window.$ || window.jQuery || window.Zepto
   if (plugin) {
-    plugin.fn.reframe = function reframePlugin(cName) {
+    plugin.fn.reframe = function reframePlugin (cName) {
       reframe(this, cName)
     }
   }
